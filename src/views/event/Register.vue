5f54e95c0436677180922b1e8b5f60e2d2ff5e2c
Sitnikov-Ryslan <sitnikov.ryslan1992@gmail.com>
2021-03-11 22:34:20 +0400

    nav 404 msg

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,22 @@
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    register() {
+      this.GStore.flashMessage = `You are succesfully registered for ${this.event.title}`
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
+      this.$router.push({
+        name: 'EventDetails'
+      })
+    }
+  }
 }
 </script>
